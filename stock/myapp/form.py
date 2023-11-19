85d5f14b4378230a0eb5af922fb4694444693d7b
Penh Seyha <penhseyha4980@gmail.com>
2023-11-19 16:13:44 +0700

    add validation

--- stock/myapp/form.py
+++ stock/myapp/form.py
@@ -6,3 +6,22 @@ class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
         fields = ['name', 'description', 'unit_price', 'quantity']
+        widgets = {
+            'name': forms.TextInput(attrs={'class': 'form-control', 'style': 'color: red;'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'rows': 3}),
+            'unit_price': forms.NumberInput(attrs={'class': 'form-control'}),
+            'quantity': forms.NumberInput(attrs={'class': 'form-control'}),
+        }
+
+    def clean(self):
+        cleaned_data = super().clean()
+        unit_price = cleaned_data.get('unit_price')
+        quantity = cleaned_data.get('quantity')
+
+        if unit_price and unit_price <= 0:
+            self.add_error('unit_price', 'Unit price must be greater than zero.')
+
+        if quantity and quantity <= 0:
+            self.add_error('quantity', 'Quantity must be greater than zero.')
+
+        return cleaned_data
